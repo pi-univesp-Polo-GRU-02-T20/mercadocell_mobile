fe6e60f67446a166245045431c97c27efe2a6454
William Tristão de Paula <william.depaula@agilecontent.com>
2022-11-15 18:31:33 -0300

    jwt token added to change password

--- src/modules/application/services.ts
+++ src/modules/application/services.ts
@@ -1,4 +1,6 @@
-import axios, { AxiosResponse } from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios, { AxiosRequestHeaders, AxiosResponse } from 'axios'
+import { STORAGE_JWT } from '../../lib/helpers/constants';
 import { JWTResponse, RequestResponse } from './models';
 import { ROOT_URL } from './settings';
 
@@ -30,12 +32,14 @@ async function login(user: string, password: string): Promise<RequestResponse<JW
 
 async function changePassword(user: string, currentPassword: string, newPassword: string): Promise<RequestResponse<JWTResponse>> {
     try {
+        let header = await getHeaderWithToken()
+
         const { data, status }: AxiosResponse<JWTResponse> = await request.put('/auth/alterarSenha', {
             login: user, 
             senha: currentPassword,
             senhaNova: newPassword
-        })
-
+        }, header)
+        
         return {
             data,
             status
@@ -49,6 +53,33 @@ async function changePassword(user: string, currentPassword: string, newPassword
         }
     }
 }
+
+async function getHeaderWithToken(): Promise<any> {
+    let header = {}
+
+    let jwt = await AsyncStorage.getItem(STORAGE_JWT)
+
+    if (jwt) {
+        header = {
+            headers: {
+                'Authorization': `Bearer ${jwt}`
+            }
+        }
+    }
+
+    return header
+}
+
+// Adicionando um interceptor de Response
+request.interceptors.response.use(function (response) {
+    // Qualquer código de status que esteja dentro de 2xx fará com que essa função seja acionada
+    // Faça algo com os dados de resposta
+    return response;
+  }, function (error) {
+    console.log("error axios interceptor", error)
+
+    return Promise.reject(error);
+  });
 
 export {
     login,
